aaf87694154b478d0a632da93e9745f8c2fd67c3
Himanshu Sharma <himshar@microsoft.com>
2023-04-05 10:59:29 +0000

    app insights

--- landingzone/main.tf
+++ landingzone/main.tf
@@ -2,3 +2,11 @@ module "resourcegroup" {
   source          = "../modules/resourcegroup"
   resource_groups = var.resource_groups
 }
+
+module "appliacation-insights" {
+  source                 = "../modules/applicationinsights"
+  resource_group_name_ai = var.resource_group_name_ai
+  application_insights   = var.application_insights
+  law_workspace_id       = var.law_workspace_id
+  appin_additional_tags  = var.appin_additional_tags
+}
--- modules/applicationinsights/variables.tf
+++ modules/applicationinsights/variables.tf
@@ -1,22 +1,11 @@
-# - Resource Group
-variable "resource_groups" {
-  description = "Resource groups"
-  type = map(object({
-    name     = string
-    location = string
-    tags     = map(string)
-  }))
-  default = {}
+variable "resource_group_name_ai" {
+  type        = string
+  description = "The name of the resource group in which the Application Insights is created"
 }
 
 ############################
 # Application Insights
 ############################
-
-variable "resource_group_name_ai" {
-  type        = string
-  description = "The name of the resource group in which the Application Insights is created"
-}
 
 variable "application_insights" {
   type = map(object({
